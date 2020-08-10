1fed0f36101768825afa29bd3e873c7196619101
Rafael Herrera <santoElias65537@gmail.com>
2020-08-09 23:26:14 -0600

    its coming along very nicely, now i need to add a checkwin function and change the function that makes board so its filled 0 - 8 instead of Es

--- src/tictac/core.clj
+++ src/tictac/core.clj
@@ -9,19 +9,45 @@
 (def state (atom (square-matrix 3 "E")))
 
 
-(defn updateBoard 
-  [y x changeTo]
-  (swap! state update-in [y x] changeTo))
+(defn vectorlengthEven?
+  [vec]
+  (even? (count vec)))
 
-(defn updateBoardV2 
-  [y x changeTo]
-  (println 
-   (str y x changeTo)))
 
-(defn updateBoardV3 
-  [y x changeTo]
-  (println state y x changeTo))
+(defn getAllE
+  [vec]
+  (filter (fn [x] (= "E" x) ) vec))
 
+
+
+(defn nextMark
+  "takes 2d vector ex. [[1 2 3] ['E' 5 'E'] ['E' 'E' 9]]"
+  [board]
+  (let [allE (mapcat getAllE board)]
+    (if (vectorlengthEven? allE)
+      (str "X")
+      (str "O"))))
+
+
+(defn getCell
+  [num]
+  (let [y (quot num 3)
+        x (- num (* y 3))]
+    (vector y x)))
+
+
+(defn updateBoard
+  [y x changeTo]
+  (swap! state assoc-in [y x] changeTo))
+
+
+(defn game
+  [num]
+  (let [cellCordinates (getCell num)
+        y (get-in cellCordinates [0])
+        x (get-in cellCordinates [1])
+        mark (nextMark @state)]
+    (updateBoard y x mark)))
 
 
 (defn acceptableAnwser?
@@ -33,22 +59,11 @@
     (not= "9" answer)));
 
 
-
-
-
-(println state)
-
-
-
-
-(defn getInputV2
+(defn getInput
   [word]
   (if (acceptableAnwser? word)
-        (updateBoardV3 word 0 "shirworkednigga")
-        (println "it didnt work")))
-
-
-(getInputV2 "2")
+      (game (Integer/parseInt word))
+      (println "it didnt work")))
 
 
 
@@ -65,72 +80,6 @@
 
 
 
-(def newState (square-matrix 3 "E"))
-
-
-(swap! state assoc-in [2 0] "dam") ;this works just remember to reset atom dumbass
-
-
-
-
-
-(defn getCell
-  [num]
-  (let [y (quot num 3)
-        x (- num (* y 3))]
-    (vector y x)))
-
-(defn updateNestedVec [nestedVec x y changeTo]
-  (assoc-in nestedVec [x y] changeTo))
-
-(def state2 (square-matrix 3 3))
-
-;this works
-;(swap! state update-in [0 0] "x")
-
-
-
-
-(defn setUp
-  [num]
-  (let ;[number (Integer/parseInt num)
-   [cellCordinates (getCell num)] cellCordinates))
-
-
-
-
-
-
-
-(defn updateNestedVec 
-  [nestedVec x y changeTo]
-  (assoc-in nestedVec [x y] changeTo))
-
-
-(defn getAllE
-  [vec]
-  (filter (fn [x] (= "E" x) ) vec))
-
-
-(defn vectorlengthEven?
-  [vec]
-  (even? (count vec)))
-
-
-
-(defn xNext?
-  "takes 2d vector ex. [[1 2 3] ['E' 5 'E'] ['E' 'E' 9]]"
-  [board]
-  (let [allE (mapcat getAllE board)]
-   (vectorlengthEven? allE)))
-
-(defn nextMark
-  "takes 2d vector ex. [[1 2 3] ['E' 5 'E'] ['E' 'E' 9]]"
-  [board]
-  (let [allE (mapcat getAllE board)]
-    (if (vectorlengthEven? allE)
-      (println "X")
-      (println "O"))))
 
 
 
@@ -152,7 +101,7 @@
 ;(idk 3 3)
 
 
-;; the one that runs from command prompt 
+;; the one that runs from command prompt
 ;; (defn getInput
 ;;   []
 ;;   (println "Enter word to find definition ->")
