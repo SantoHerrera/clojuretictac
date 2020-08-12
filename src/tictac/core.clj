a257f5dc1a5454f229b400012553c354b7ff36f6
Rafael Herrera <santoElias65537@gmail.com>
2020-08-11 22:20:29 -0600

    got the checkwin function from another project I read on github, just need to plug it in if you will. and make some changes to state because checkwin will return true if I dont

--- src/tictac/core.clj
+++ src/tictac/core.clj
@@ -1,24 +1,27 @@
 (ns tictac.core
   (:gen-class))
 
+;https://github.com/tbaik/clojure-ttt
+;the ttt version I read before making this.
+;I read it, liked it. Tried to forget it.
+;then wrote this.
+;I guessing they are very similer.
+;if you want to go and read the original.
+
+
 (defn square-matrix
   [n p]
   (->> p (repeat n) vec (repeat n) vec))
 
-
 (def state (atom (square-matrix 3 "E")))
-
 
 (defn vectorlengthEven?
   [vec]
   (even? (count vec)))
 
-
 (defn getAllE
   [vec]
   (filter (fn [x] (= "E" x) ) vec))
-
-
 
 (defn nextMark
   "takes 2d vector ex. [[1 2 3] ['E' 5 'E'] ['E' 'E' 9]]"
@@ -28,18 +31,15 @@
       (str "X")
       (str "O"))))
 
-
 (defn getCell
   [num]
   (let [y (quot num 3)
         x (- num (* y 3))]
     (vector y x)))
 
-
 (defn updateBoard
   [y x changeTo]
   (swap! state assoc-in [y x] changeTo))
-
 
 (defn game
   [num]
@@ -49,7 +49,6 @@
         mark (nextMark @state)]
     (updateBoard y x mark)))
 
-
 (defn acceptableAnwser?
   "returns true if its a string digit thats between 0 - 8"
   [answer]
@@ -58,12 +57,80 @@
     (every? #(Character/isDigit %) answer);if character can be turned into number
     (not= "9" answer)));
 
-
 (defn getInput
   [word]
   (if (acceptableAnwser? word)
       (game (Integer/parseInt word))
       (println "it didnt work")))
+
+
+
+
+
+
+
+
+
+(defn contains-same-pieces
+  [coll]
+  (apply = coll))
+
+
+
+(defn has-winner-from-separation?
+  [separated-coll]
+  (some true? (map contains-same-pieces separated-coll)))
+
+(defn separate-into-rows
+  [width board]
+  (partition width board))
+
+
+(defn has-horizontal-winner?
+  [width board]
+  (has-winner-from-separation? (separate-into-rows width board)))
+
+(defn separate-into-columns
+  [width board]
+  (apply map vector (separate-into-rows width board)))
+
+
+(defn has-vertical-winner? [width board]
+    (has-winner-from-separation? (separate-into-columns width board)))
+
+
+
+
+(defn board-width
+   [board]
+   (int (Math/sqrt (count board))))
+
+(defn valid-moves
+  [board]
+  (filter integer? board))
+
+
+(defn has-winner?
+   [board]
+;board -> [1 2 3 4 5 x o 8 9]
+   (let [width (board-width board)]
+        (and (< (count (valid-moves board)) 5) ;im guessing its becuase no one could win with less than 5 pieces on board
+             (or (has-horizontal-winner? width board);lit
+                 (has-vertical-winner? width board)))));nice
+
+
+(def board ["x" "x" "x" 3 4 5 6 "o" "o"])
+
+(< (count (valid-moves board)) 5)
+
+
+(has-horizontal-winner? (board-width board) board)
+
+(has-winner? ["x" "x" "x"])
+
+
+
+
 
 
 
