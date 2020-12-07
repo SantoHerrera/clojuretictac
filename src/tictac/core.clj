9d41988d597d4e09393e7212d4f605ece8fbfde8
Rafael Herrera <santoElias65537@gmail.com>
2020-12-06 17:25:11 -0700

    some clean up and pluging things back. VErsion 1.0 is near

--- src/tictac/core.clj
+++ src/tictac/core.clj
@@ -73,9 +73,21 @@
   "returns true if its a string digit thats between 0 - 8"
   [answer]
   (and
+    (not= false (string? answer))
     (= 1 (count answer));if string is only one character
     (every? #(Character/isDigit %) answer);if character can be turned into number
     (not= "9" answer)))
+
+
+(defn return-acceptable-answer
+  [answer]
+  (if (and
+       (= 1 (count answer))
+       (every? #(Character/isDigit %) answer)
+       (not= "9" answer))
+     (Integer/parseInt answer)
+     9))
+
 
 (defn print-board
   [board]
@@ -98,92 +110,39 @@
   []
   (System/exit 0))
 
-; (defn new-game
-;   []
-;   (loop [all-inputs []
-;          game newState
-;          moves-available (moves-available game)
-;          current-input (read-line)]
-;     (print-board game)
-;     (cond
-;       (exit-game? current-input) (exit-game)
-;       (has-winner? game) (println "fuck yeah youve won ")
-;       (= 0 (moves-available game)) (println "nobody fucking won"))
-;     (recur (cons all-inputs current-input)
-;        (update-board (Integer/parseInt
-;                       ;(if (acceptable-answer? current-input) current-input "9"))
-;                       current-input)
-;         (next-mark game) game) (dec moves-available))))
-
-
-
-
-(defn new-gameV2
+(defn get-input
   []
-  (loop [input (read-line)
+  (read-line))
+
+
+(defn new-game
+  []
+  (loop [input "9"
          game newState
-         moves-available (moves-available game)]
+         moves-availabless (moves-available game)]
         (print-board game)
+        ;(println moves-availabless)
         (cond
           (exit-game? input) (exit-game)
-          (has-winner? game) (println "fuck yeah youve won ")
-          (= 0 (moves-available game)) (println "nobody fucking won"))
-        (recur (read-line) (update-board (Integer/parseInt input) (next-mark game) game) (dec moves-available))))
-
-(def hasWinnerTest [["x" "x" "x"] [3 4 5] [6 7 8]])
-(println hasWinnerTest)
-
-(defn new-gameV3
-  []
-  (loop [all-inputs []
-         game newState
-         moves-available (moves-available game)]
-    (print-board game)
-    (let [current-input (read-line)]
-     (println all-inputs
-      (cond
-        (exit-game? current-input) (println "it works from here on out")
-        (has-winner? game) (println "fuck yeah youve won ")
-        (= 0 (moves-available game)) (println "nobody fucking won"))
-      (recur (cons all-inputs current-input) (update-board
-                                              (Integer/parseInt (if (acceptable-answer? current-input) current-input) "9")
-                                              (next-mark game) game)
-                                         (dec moves-available))))))
-
-
-(defn new-gameV3
-  []
-  (loop [game newState
-         moves-available (moves-available game)]
-    (print-board game)
-    (case (read-line)
-      "e" (println "exits game")
-      "0" (println "o"))
-    (recur (update-board
-             (Integer/parseInt input)
-             (next-mark game) game
-             (dec moves-available)))))
-
-
-
-(defn new-gameV4
-  [])
-
-
-
-
-;inplement this
-(defn main-loop []
-  (case (read-line)
-   "q" nil
-   "a" (do (println "got a command!") (recur))
-   "b" (do (println "got b command!") (recur))
-     (do (println "got invalid command!") (recur))))
+          (has-winner? game) (println "fucking won"))
+        (recur (read-line)
+               (update-board (return-acceptable-answer input) (next-mark game) game)
+               (moves-available game))))
 
 
 (defn -main
-  [& args]
-  (new-gameV3))
+  []
+  (new-game))
+
+
+
+
+
+
+
+
+
+
 
 
 
